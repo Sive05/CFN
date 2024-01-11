e991095d31e474f0193c2eb4d231975de0988299
Sive05 <139841723+Sive05@users.noreply.github.com>
2024-01-11 19:16:10 +0530

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = "Continue"
 $name = "VpcFromCfn"
 $templatebody = get-content cfn.yml -Raw
 
-if (get-CFNStack -stackname $name){
-    update-CFNStack -StackName $name -TemplateBody $templatebody
-} else {
+#if (get-CFNStack -stackname $name){
+#    update-CFNStack -StackName $name -TemplateBody $templatebody
+#} else {
     New-CFNStack -StackName $name -TemplateBody $templatebody
-}
+#}
